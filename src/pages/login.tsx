3cf10640597e3baaae85d9a088e84566bc34b8d3
EricaSugui <ericasugui@gmail.com>
2024-02-21 12:41:19 -0300

    chore: adjust Header for login page and login component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,11 @@
+import PizzaHeaderDesktop from "@/ui/components/PizzaHeaderDesktop/PizzaHeaderDesktop";
 import PizzaLogin from "@/ui/components/PizzaLogin/PizzaLogin";
 
 export default function Login() {
     return(
-       <PizzaLogin />
+        <>
+            <PizzaHeaderDesktop />
+            <PizzaLogin />
+        </>
     )
 }
