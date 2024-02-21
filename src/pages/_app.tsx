0af6d1db404e1c1ed367dbf6542dc113087f18f2
EricaSugui <ericasugui@gmail.com>
2024-02-21 12:19:26 -0300

    remove header and footer from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
-import PizzaFooterDesktop from "@/ui/components/PizzaFooterDesktop/PizzaFooterDesktop";
-import PizzaHeaderDesktop from "@/ui/components/PizzaHeaderDesktop/PizzaHeaderDesktop";
 import "@/ui/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(<>
-    <PizzaHeaderDesktop />
+  return(
     <Component {...pageProps} />
-    <PizzaFooterDesktop />
-  </>
   ) 
 }
