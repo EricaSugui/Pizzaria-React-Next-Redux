de60a5abc3e7827be51eaae64bad0ef987f8978f
EricaSugui <ericasugui@gmail.com>
2024-02-21 18:53:27 -0300

    chore: adjust logic for PizzaPage component

--- src/ui/components/PizzaPage/PizzaPage.tsx
+++ src/ui/components/PizzaPage/PizzaPage.tsx
@@ -8,10 +8,41 @@ import Minus from '/public/img/Minus.svg'
 import Plus from '/public/img/Plus.svg'
 import PizzaHeaderArrowBack from "../PizzaHeaderArrowBack/PizzaHeaderArrowBack";
 import PizzaFooter from "../PizzaFooter/PizzaFooter";
+import { useState } from "react";
 
 export default function PizzaPage({pizza}: {pizza: IPizzaList}){
+    const [ qtd, setQtd ] = useState(1);
+    const [ order, setOrder ] = useState<IPizzaList[]>([]);
+
+    const handleMinus = () => {
+        if(qtd < 1){
+            return qtd
+        }
+        setQtd(qtd - 1)
+    }
+    const handlePlus = () => {
+        if(qtd > 9){
+            return qtd
+        }
+        setQtd(qtd + 1)
+    }
+
+    const orderItem: IPizzaList = 
+        {
+            id: pizza.id,
+            title: pizza.title,
+            picture: pizza.picture,
+            qtd: qtd, 
+            finalPrice: pizza.price * qtd
+        }
+    const handleOrder = () => {
+        setOrder([...order, orderItem])
+    }
+    console.log("order: ", order)
+    
     return (
-            <><PizzaHeaderArrowBack />
+            <>
+            <PizzaHeaderArrowBack />
         <div className={styles['pizza-container']}>
             <div className={styles['pizza-picture']}>
                 <Image src={pizza.picture} alt={pizza.title} />
@@ -20,6 +51,7 @@ export default function PizzaPage({pizza}: {pizza: IPizzaList}){
             <div className={styles['pizza-main']}>
 
                 <h2 className={styles['pizza-title']}>{pizza.title}</h2>
+
                 <div className={styles['pizza-price']}>
                     <div className="mt-2 flex items-center text-m text-gray-500 gap-2">
                         <Image src={Rating} alt={`rating: ${pizza.rating}`} />
@@ -34,22 +66,25 @@ export default function PizzaPage({pizza}: {pizza: IPizzaList}){
                         {pizza.kcal}
                     </div>
                 </div>
+
                 <div className={styles['pizza-content']}>
                     {pizza.description}
                 </div>
+
                 <hr />
+
                 <div className={styles['pizza-price']}>
-                    <button>
+                    <button onClick={handleMinus}>
                         <Image src={Minus} alt="diminuir quantidade"/>
                     </button>
-                    <div className={styles['pizza-qtd']}>1</div>
-                    <button>
+                    <div className={styles['pizza-qtd']}>{qtd}</div>
+                    <button onClick={handlePlus}>
                         <Image src={Plus} alt="aumentar quantidade"/>
                     </button>
-                    <p>R$ {pizza.price}</p>
+                    <p>R$ {pizza.price?.toFixed(2)}</p>
                 </div>
                 <div className={styles['pizza-submit']}>
-                    <button>ADICIONAR AO CARRINHO</button>
+                    <button onClick={handleOrder}>ADICIONAR AO CARRINHO</button>
                 </div>  
 
             </div>
