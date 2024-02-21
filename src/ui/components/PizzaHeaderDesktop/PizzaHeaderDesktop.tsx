c23e46c30fde746c3b9d5a2ffd2608250552da02
EricaSugui <ericasugui@gmail.com>
2024-02-21 12:21:20 -0300

    add children prop to Desktop Header

--- src/ui/components/PizzaHeaderDesktop/PizzaHeaderDesktop.tsx
+++ src/ui/components/PizzaHeaderDesktop/PizzaHeaderDesktop.tsx
@@ -1,23 +1,30 @@
 import Image from 'next/image'
 import style from './PizzaHeaderDesktop.module.css'
 import margerita from '/public/img/margerita.png'
+import Cart from '/public/img/faShoppingCart.svg'
+import User from '/public/img/faUserAlt.svg'
 import Link from 'next/link'
-import NavMenu from '../NavMenu/NavMenu'
+import {INavProps} from '@/data/@types/INavProps'
 
-export default function PizzaHeaderDesktop(){
+export default function PizzaHeaderDesktop({children}: INavProps){
     return (
         <div className={style['pizza-desktop']}>
-            <Link href={`/`}>
-                <div className={style['pizza-logo']}>
-                    <Image src={margerita} alt='Pizzaria React'/>
-                    <div className={style['pizza-logo-name']}>
-                        <h1>Pizza React</h1>
-                        <p>Pizzaria com NextJS</p>
+            <div className={style['pizza-header']}>
+                <Link href={`/`}>
+                    <div className={style['pizza-logo']}>
+                        <Image src={margerita} alt='Pizzaria React'/>
+                        <div className={style['pizza-logo-name']}>
+                            <h1>Pizza React</h1>
+                            <p>Pizzaria com NextJS</p>
+                        </div>
                     </div>
+                </Link>
+                <div className={style['pizza-header-right']}>
+                    <Image src={Cart} alt='carrinho de compras' />
+                    <Image src={User} alt='sua conta' />
                 </div>
-            </Link>
-            
-            <NavMenu/>            
+            </div>
+            {children}
         </div>
     )
 }
