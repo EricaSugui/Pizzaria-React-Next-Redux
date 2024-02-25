b8f4fdf38b96b77d4a33a33059334a4572c21dbf
EricaSugui <ericasugui@gmail.com>
2024-02-25 18:46:34 -0300

    chore: add redux store to PizzaPage page and component

--- src/pages/produtos/pizzas/[slug].tsx
+++ src/pages/produtos/pizzas/[slug].tsx
@@ -1,15 +1,21 @@
 import PizzaPage from "@/ui/components/PizzaPage/PizzaPage";
 import { useRouter } from 'next/router'
+
 import useIndex from "@/data/hooks/pages/useIndex";
 import PizzaHeaderDesktop from "@/ui/components/PizzaHeaderDesktop/PizzaHeaderDesktop";
 
-export default function Pizzas(){
+function Pizzas(){
     const router = useRouter();
     const { slug } = router.query
     const { pizzas } = useIndex();
     const pizzaItem = pizzas.filter(pizza => pizza.slug === slug)
+    
     return <>
         <PizzaHeaderDesktop />
         <PizzaPage pizza={pizzaItem[0]}/>
     </>
 }
+
+
+
+export default Pizzas;
--- src/ui/components/PizzaPage/PizzaPage.tsx
+++ src/ui/components/PizzaPage/PizzaPage.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+
+import { connect } from 'react-redux'
+import { addItem } from '../../../store/actions/item'
+import {useDispatch} from 'react-redux'
+
 import Image from "next/image";
 import styles from './PizzaPage.module.css';
 import { IPizzaList } from "@/data/@types/IPizzaList";
@@ -13,11 +18,14 @@ import YellowFavorite from '/public/img/favYellow.svg'
 import PizzaHeaderArrowBack from "../PizzaHeaderArrowBack/PizzaHeaderArrowBack";
 import PizzaFooter from "../PizzaFooter/PizzaFooter";
 
-export default function PizzaPage({pizza}: {pizza: IPizzaList}){
+function PizzaPage(
+    pizzasList?: IPizzaList[],
+    pizza?: {pizza: IPizzaList}
+    ){
     const [ qtd, setQtd ] = useState(1);
     const [ fav, setFav ] = useState(false);
-    const [ order, setOrder ] = useState<IPizzaList[]>([]);
     const router = useRouter(); 
+    const dispatch = useDispatch()
     
     const handleMinus = () => {
         if(qtd < 1){
@@ -34,22 +42,21 @@ export default function PizzaPage({pizza}: {pizza: IPizzaList}){
 
     const orderItem: IPizzaList = 
         {
-            id: pizza.id,
-            title: pizza.title,
-            picture: pizza.picture,
-            qtd: qtd, 
-            finalPrice: pizza.price * qtd,
+            id: pizzasList.pizza.id,
+            title: pizzasList.pizza.title,
+            picture: pizzasList.pizza.picture,
+            qtd: qtd,
+            price: pizzasList.pizza.price,
+            finalPrice: pizzasList.pizza.price * qtd,
             fav: fav,
         }
     const handleOrder = () => {
-        setOrder([...order, orderItem]);
+        dispatch(addItem(orderItem))
         router.push("/carrinho")
     }
     const handleFavorite = () => {
         setFav(!fav)
     }
-    console.log("order: ", order)
-    console.log('fav: ', fav)
     
     return (
             <>
@@ -58,32 +65,33 @@ export default function PizzaPage({pizza}: {pizza: IPizzaList}){
                     fav ? (<Image src={YellowFavorite} alt="favorito"/>) : (<Image src={Favorite} alt="favorito"/>)
                 }</button>
             </PizzaHeaderArrowBack>
+
         <div className={styles['pizza-container']}>
             <div className={styles['pizza-picture']}>
-                <Image src={pizza.picture} alt={pizza.title} priority={true} />
+                <Image src={pizzasList.pizza.picture} alt={pizzasList.pizza.title} priority={true} />
             </div>
 
             <div className={styles['pizza-main']}>
 
-                <h2 className={styles['pizza-title']}>{pizza.title}</h2>
+                <h2 className={styles['pizza-title']}>{pizzasList.pizza.title}</h2>
 
                 <div className={styles['pizza-price']}>
                     <div className="mt-2 flex items-center text-m text-gray-500 gap-2">
-                        <Image src={Rating} alt={`rating: ${pizza.rating}`} />
-                        {pizza.rating}
+                        <Image src={Rating} alt={`rating: ${pizzasList.pizza.rating}`} />
+                        {pizzasList.pizza.rating}
                     </div>
                     <div className="mt-2 flex items-center text-m text-gray-500 gap-2">
-                        <Image src={Clock} alt={`time to deliver: ${pizza.time}`} />
-                        {pizza.time}
+                        <Image src={Clock} alt={`time to deliver: ${pizzasList.pizza.time}`} />
+                        {pizzasList.pizza.time}
                     </div>
                     <div className="mt-2 flex items-center text-m text-gray-500 gap-2">
-                        <Image src={Kcal} alt={`Kcal: ${pizza.kcal}`} />
-                        {pizza.kcal}
+                        <Image src={Kcal} alt={`Kcal: ${pizzasList.pizza.kcal}`} />
+                        {pizzasList.pizza.kcal}
                     </div>
                 </div>
 
                 <div className={styles['pizza-content']}>
-                    {pizza.description}
+                    {pizzasList.pizza.description}
                 </div>
 
                 <hr />
@@ -96,7 +104,7 @@ export default function PizzaPage({pizza}: {pizza: IPizzaList}){
                     <button onClick={handlePlus}>
                         <Image src={Plus} alt="aumentar quantidade"/>
                     </button>
-                    <p>R$ {pizza.price?.toFixed(2)}</p>
+                    <p>R$ {pizzasList.pizza.price?.toFixed(2)}</p>
                 </div>
                 <div className={styles['pizza-submit']}>
                     <button onClick={handleOrder}>ADICIONAR AO CARRINHO</button>
@@ -105,6 +113,16 @@ export default function PizzaPage({pizza}: {pizza: IPizzaList}){
             </div>
 
             <PizzaFooter />          
-        </div></>
+        </div>
+    </>
     )
 }
+
+const mapStateToProps = (state: any) => ({
+    pizzasList: state.item.list
+})
+
+export default connect(
+    mapStateToProps, 
+    { addItem }
+)(PizzaPage)
